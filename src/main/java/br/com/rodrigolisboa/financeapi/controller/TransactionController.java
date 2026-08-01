7c76571f2a039a378d1adf3c235f91023ef642d6
Rodrigo Lisboa Iachety <rodrigolisboaiachety@gmail.com>
2026-08-01 14:11:32 -0300

    feat: implement complete transaction CRUD

--- src/main/java/br/com/rodrigolisboa/financeapi/controller/TransactionController.java
+++ src/main/java/br/com/rodrigolisboa/financeapi/controller/TransactionController.java
@@ -1,5 +1,4 @@
 package br.com.rodrigolisboa.financeapi.controller;
-
 import br.com.rodrigolisboa.financeapi.entity.Transaction;
 import br.com.rodrigolisboa.financeapi.service.TransactionService;
 import org.springframework.http.HttpStatus;
@@ -8,6 +7,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @RequestMapping("/transactions")
@@ -23,5 +29,23 @@ public class TransactionController {
     @ResponseStatus(HttpStatus.CREATED)
     public Transaction create(@RequestBody Transaction transaction) {
         return service.save(transaction);
+    }
+    @GetMapping
+    public List<Transaction> findAll() {
+        return service.findAll();
+    }
+    @PutMapping("/{id}")
+    public Transaction update(@PathVariable Long id,
+                              @RequestBody Transaction transaction) {
+        return service.update(id, transaction);
+    }
+    @GetMapping("/{id}")
+    public Transaction findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
     }
 }
--- src/main/java/br/com/rodrigolisboa/financeapi/service/TransactionService.java
+++ src/main/java/br/com/rodrigolisboa/financeapi/service/TransactionService.java
@@ -3,6 +3,7 @@ package br.com.rodrigolisboa.financeapi.service;
 import br.com.rodrigolisboa.financeapi.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
 import br.com.rodrigolisboa.financeapi.entity.Transaction;
+import java.util.List;
 
 @Service
 public class TransactionService {
@@ -14,5 +15,24 @@ public class TransactionService {
     }
     public Transaction save(Transaction transaction) {
         return repository.save(transaction);
+    }
+    public List<Transaction> findAll() {
+        return repository.findAll();
+    }
+    public Transaction update(Long id, Transaction transaction) {
+        Transaction existingTransaction = repository.findById(id).orElseThrow();
+
+        existingTransaction.setDescription(transaction.getDescription());
+        existingTransaction.setAmount(transaction.getAmount());
+        existingTransaction.setType(transaction.getType());
+        existingTransaction.setDate(transaction.getDate());
+
+        return repository.save(existingTransaction);
+    }
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+    public Transaction findById(Long id) {
+        return repository.findById(id).orElseThrow();
     }
 }
